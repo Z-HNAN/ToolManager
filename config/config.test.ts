9f33a55289f861267d65bc7f6a1efd7a0340d3f5
Z-HNAN <zhn68033@163.com>
2020-02-14 22:58:18 +0800

    feat 更换baseURL为https

--- config/config.test.ts
+++ config/config.test.ts
@@ -2,7 +2,7 @@ import { IConfig } from 'umi-types'
 
 const config: IConfig = {
   define: {
-    'process.env.base_url': 'http://yapi.hdutu.xyz/mock/44/tsm/'
+    'process.env.base_url': 'https://yapi.hdutu.xyz/mock/44/tsm/'
   },
 }
 
